9660da553c438ec96fc306b687152577a8b38524
Verashiro17 <chenshiqi7@shein.com>
2023-05-05 16:13:22 +0800

    feat:设置自动缩进为4个空格

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function component(){
-  var element = document.createElement('p');
-  element.innerHTML = ('Hello world');
-  return element;
+    var element = document.createElement('p');
+    element.innerHTML = ('Hello world');
+    return element;
 }
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-var path =require('path');
+let path = require('path');
 module.exports = {
-  mode:'development',//开发模式
-  entry:'./src/index.js', //入口文件
-  output: {  //出口文件
-    path:path.resolve(__dirname,'dist'),
-    filename:'bundle.js',
-  },
-  devServer:{
-    host: 'localhost',
-    compress: true,
-    port: 1717
-  }
+    mode:'development',//开发模式
+    entry:'./src/index.js', //入口文件
+    output: {  //出口文件
+        path:path.resolve(__dirname,'dist'),
+        filename:'bundle.js',
+    },
+    devServer:{
+        host: 'localhost',
+        compress: true,
+        port: 1717
+    }
 }
